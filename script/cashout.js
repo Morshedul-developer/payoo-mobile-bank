d3b236ebdbb6b66a209cacd56ebbfcdc79413300
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-18 22:05:13 +0600

    made the functionality for the cashout part

--- script/cashout.js
+++ script/cashout.js
@@ -1,4 +1,30 @@
 document.getElementById('cashout-btn')
     .addEventListener('click',function(){
-        console.log("Hello");
+        const agentNumber = document.getElementById('agent-input').value;
+
+        if(agentNumber.length != 11) {
+            alert("Invalid Agent Number...");
+            return;
+        }
+
+        const mainBalance = document.getElementById('balance');
+        const balance = mainBalance.innerText;
+        const cashoutAmount = document.getElementById('amount-input').value;
+        const newBalance = Number(balance) - Number(cashoutAmount);
+        
+        if(newBalance < 0) {
+            alert("Invalid amount...");
+            return;
+        }
+
+        const pin = document.getElementById('pin-input').value;
+
+        if(pin == '1234') {
+            alert('Cashout successful...');
+            mainBalance.innerText = newBalance;
+        }
+        else {
+            alert('Invalid PIN...');
+            return;
+        }
     });
